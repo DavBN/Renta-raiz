5986d0048f1b4a0c408efa4d2144efadff6854e5
DavBN <162839191+DavBN@users.noreply.github.com>
2024-08-09 12:06:55 -0500

    Maquetación de la navegación para todos los dispositivos, componentes tocados - header,  navbar, se hace un mapeo por los datos del header que están en el componente datatypes y los traemos por medio de la función.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { CiMenuFries } from "react-icons/ci"
+import { BsTelephone } from "react-icons/bs"
 import Link from "next/link"
 import { Navbar } from "../Navbar"
 import { useState } from "react"
@@ -15,7 +16,15 @@ export function Header() {
                     <h1>Bienes Raices OSC</h1>
                 </Link>
                 <CiMenuFries className="block text-2xl md:hidden" onClick={() => setOpenMobileMenu(!openMobileMenu)} />
-                <Navbar />
+                <Navbar openMobileMenu={openMobileMenu} />
+
+                <div className="flex items-center gap-2 md:gap-5">
+                    <Link href="tel:3196168502" className="flex items-center gap-4 cursor-pointer">
+                      <BsTelephone/>
+                      <span className="hidden md:block">+57 319 616 8502</span>
+                    </Link>
+                    <Link href="/login" className="px-3 py-2 text-white rounded-lg bg-orange-500 hover:bg-black">Login</Link>
+                </div>
             </div>
         </div>
     )
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react"
 import { dataHeader } from "../Header/Header.data"
-export function Navbar() {
-
+import { NavbarProps } from "./Navbar.types"
+import Link from "next/link";
+export function Navbar(props: NavbarProps) {
+    const { openMobileMenu } = props;
     return (
-        <p>Navbar</p>
+        <div className={`${openMobileMenu ? 'absolute z[1] left-0 top-20 bg-white r-0 w-full px-4 py-4' : 'hidden'} gap-5 md:flex`}>
+           {dataHeader.map(({id, name, link}) => (
+            <Link key={id} href={link} className="block hover:text-orange-500 hover:border-b-[1px]">{name}</Link>
+           ))}
+        </div>
     )
 }
