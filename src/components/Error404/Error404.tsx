220d4d3e8afb30f02b5a749716e0a4aec5748a40
DavBN <162839191+DavBN@users.noreply.github.com>
2024-08-27 15:33:19 -0500

    Proyecto terminado, los botones no son funcionales ya que no cuenta con una base de datos, el menu desplegable en versión de pc todos los links dirigen a la home, se creo una página 404 para errores

--- src/components/Error404/Error404.tsx
+++ src/components/Error404/Error404.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export function Error404() {
     return (
-        <p>Error 404</p>
+        <div className="flex flex-col items-center justify-center min-h-screen text-center px-5">
+            <h1 className="text-5xl font-semibold mb-6">Error 404</h1>
+            <h2 className="text-3xl mb-5">¡La página que buscas no existe!</h2>
+            <Image src="/assets/404-not-found.avif" alt="No existe" width={400} height={400} className="rounded-lg shadow-light"/>
+            <Link href="/" className="m-5 px-3 py-2 bg-orange-400 text-white rounded-lg">Volver a la página principal</Link>
+        </div>
     )
 }
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,59 @@
+import Image from "next/image";
+
 export function Form() {
     return (
-        <p>Form</p>
+        <div className="px-3">
+
+            <div className="py-4 px-3 rounded-lg shadow-light">
+                <div className="flex-gap-4">
+                    <Image src="/assets/comercial.png" alt="Vendedor" width={100} height={100} className="border-2 rounded-full border-orange-400" />
+                    <div>
+                        <p>Oscar Gallego</p>
+                        <p className="text-orange-400 font-semibold">Asesor Comercial</p>
+                    </div>
+                </div>
+
+                <div className="my-5">
+                    <div>
+                        <label className="text-sm text-orange-500">Nombre</label>
+                        <div className="mt-2">
+                            <input type="text" name="name"
+                                className="w-full rounded-md border-0 py-1.5 text-orange-400 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-orange-500 focus:ring-0 focus:ring-inset text-sm" />
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-2">
+                    <label className="text-sm text-orange-500">
+                        Teléfono
+                    </label>
+                    <div>
+                        <input
+                            id="phone"
+                            name="phone"
+                            type="phone"
+                            autoComplete="phone"
+                            className="w-full rounded-md border-0 py-1.5 text-orange-400 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-orange-500 focus:ring-0 focus:ring-inset text-sm"
+                        />
+                    </div>
+                </div>
+                <div className="mt-2">
+                    <label className="text-sm text-orange-500">
+                        Descripción
+                    </label>
+                    <div className="mt-2">
+                        <textarea
+                            name="description"
+                            rows={3}
+                            className="w-full rounded-md border-0 py-1.5 text-orange-400 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-orange-500 focus:ring-0 focus:ring-inset text-sm"
+                            defaultValue={''}
+                       />
+                    </div>
+                </div>
+                <div className="flex justify-between gap-5 my-4">
+                    <button className="bg-orange-400 px-4 py-2 text-white rounded-lg text-sm">Enviar</button>
+                    <button className="border-[1px] border-orange-400 text-orange-300 px-2 py-2 rounded-lg text-sm">Llamar</button>
+                </div>
+            </div>
+        </div>
     )
 }
