bd0529c21a74bed5081ceb2794c5220598296952
DavBN <162839191+DavBN@users.noreply.github.com>
2024-08-27 14:12:05 -0500

    Footer y componente de unete a nosotros terminado

--- src/components/JoinCommunity/JoinCommunity.tsx
+++ src/components/JoinCommunity/JoinCommunity.tsx
@@ -1,5 +1,16 @@
+import { Transition } from "../Transition";
+import { FaRegPaperPlane } from "react-icons/fa";
+
 export function JoinCommunity() {
     return (
-        <p>Join community</p>
+        <Transition className="grid items-center px-4 py-8 md:py-44 md:px-36 md:grid-cols-2 bg-orange-500">
+            <h3 className="max-w-lg text-4xl font-semibold text-white">Únete y descubre la magía de raíz</h3>
+            <div className="mx-auto mt-5 md:max-auto">
+                <button className="flex items-center justify-between py-5 text-white transition-all duration-100 bg-black rounded-lg px-7 w-fit hover:bg-black/70 ">
+                  <FaRegPaperPlane/>
+                  <span className="ml-3">Únete a nosotros</span>
+                </button>
+            </div>
+        </Transition>
     )
 }
